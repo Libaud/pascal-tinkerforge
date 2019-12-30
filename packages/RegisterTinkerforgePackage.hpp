b5d78ef44db2eeea54a7c82d0849b2f49fe8f480
Frédéric Libaud <frederic.libaud@numx-sets.eu>
2019-12-30 14:18:54 +0100

    third commit: adding C++ Builder dedicated package project

---
+++ packages/RegisterTinkerforgePackage.hpp
@@ -0,0 +1,175 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2018 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'RegisterTinkerforgePackage.pas' rev: 33.00 (Windows)
+
+#ifndef RegistertinkerforgepackageHPP
+#define RegistertinkerforgepackageHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>
+#include <SysInit.hpp>
+#include <System.Classes.hpp>
+#include <System.SysUtils.hpp>
+#include <BrickDC.hpp>
+#include <BrickHAT.hpp>
+#include <BrickHATZero.hpp>
+#include <BrickIMU.hpp>
+#include <BrickIMUV2.hpp>
+#include <BrickletAccelerometer.hpp>
+#include <BrickletAccelerometerV2.hpp>
+#include <BrickletAirQuality.hpp>
+#include <BrickletAmbientLight.hpp>
+#include <BrickletAmbientLightV2.hpp>
+#include <BrickletAmbientLightV3.hpp>
+#include <BrickletAnalogIn.hpp>
+#include <BrickletAnalogInV2.hpp>
+#include <BrickletAnalogInV3.hpp>
+#include <BrickletAnalogOut.hpp>
+#include <BrickletAnalogOutV2.hpp>
+#include <BrickletAnalogOutV3.hpp>
+#include <BrickletBarometer.hpp>
+#include <BrickletBarometerV2.hpp>
+#include <BrickletCAN.hpp>
+#include <BrickletCANV2.hpp>
+#include <BrickletCO2.hpp>
+#include <BrickletCO2V2.hpp>
+#include <BrickletColor.hpp>
+#include <BrickletColorV2.hpp>
+#include <BrickletCompass.hpp>
+#include <BrickletCurrent12.hpp>
+#include <BrickletCurrent25.hpp>
+#include <BrickletDistanceIR.hpp>
+#include <BrickletDistanceIRV2.hpp>
+#include <BrickletDistanceUS.hpp>
+#include <BrickletDistanceUSV2.hpp>
+#include <BrickletDMX.hpp>
+#include <BrickletDualButton.hpp>
+#include <BrickletDualButtonV2.hpp>
+#include <BrickletDualRelay.hpp>
+#include <BrickletDustDetector.hpp>
+#include <BrickletEnergyMonitor.hpp>
+#include <BrickletEPaper296x128.hpp>
+#include <BrickletGPS.hpp>
+#include <BrickletGPSV2.hpp>
+#include <BrickletHallEffect.hpp>
+#include <BrickletHallEffectV2.hpp>
+#include <BrickletHumidity.hpp>
+#include <BrickletHumidityV2.hpp>
+#include <BrickletIndustrialAnalogOut.hpp>
+#include <BrickletIndustrialAnalogOutV2.hpp>
+#include <BrickletIndustrialCounter.hpp>
+#include <BrickletIndustrialDigitalIn4.hpp>
+#include <BrickletIndustrialDigitalIn4V2.hpp>
+#include <BrickletIndustrialDigitalOut4.hpp>
+#include <BrickletIndustrialDigitalOut4V2.hpp>
+#include <BrickletIndustrialDual020mA.hpp>
+#include <BrickletIndustrialDual020mAV2.hpp>
+#include <BrickletIndustrialDualAnalogIn.hpp>
+#include <BrickletIndustrialDualAnalogInV2.hpp>
+#include <BrickletIndustrialDualRelay.hpp>
+#include <BrickletIndustrialQuadRelay.hpp>
+#include <BrickletIndustrialQuadRelayV2.hpp>
+#include <BrickletIO4.hpp>
+#include <BrickletIO4V2.hpp>
+#include <BrickletIO16.hpp>
+#include <BrickletIO16V2.hpp>
+#include <BrickletIsolator.hpp>
+#include <BrickletJoystick.hpp>
+#include <BrickletJoystickV2.hpp>
+#include <BrickletLaserRangeFinder.hpp>
+#include <BrickletLaserRangeFinderV2.hpp>
+#include <BrickletLCD16x2.hpp>
+#include <BrickletLCD20x4.hpp>
+#include <BrickletLCD128x64.hpp>
+#include <BrickletLEDStrip.hpp>
+#include <BrickletLEDStripV2.hpp>
+#include <BrickletLine.hpp>
+#include <BrickletLinearPoti.hpp>
+#include <BrickletLinearPotiV2.hpp>
+#include <BrickletLoadCell.hpp>
+#include <BrickletLoadCellV2.hpp>
+#include <BrickletMoisture.hpp>
+#include <BrickletMotionDetector.hpp>
+#include <BrickletMotionDetectorV2.hpp>
+#include <BrickletMotorizedLinearPoti.hpp>
+#include <BrickletMultiTouch.hpp>
+#include <BrickletMultiTouchV2.hpp>
+#include <BrickletNFC.hpp>
+#include <BrickletNFCRFID.hpp>
+#include <BrickletOLED64x48.hpp>
+#include <BrickletOLED128x64.hpp>
+#include <BrickletOLED128x64V2.hpp>
+#include <BrickletOneWire.hpp>
+#include <BrickletOutdoorWeather.hpp>
+#include <BrickletParticulateMatter.hpp>
+#include <BrickletPiezoBuzzer.hpp>
+#include <BrickletPiezoSpeaker.hpp>
+#include <BrickletPiezoSpeakerV2.hpp>
+#include <BrickletPTC.hpp>
+#include <BrickletPTCV2.hpp>
+#include <BrickletRealTimeClock.hpp>
+#include <BrickletRealTimeClockV2.hpp>
+#include <BrickletRemoteSwitch.hpp>
+#include <BrickletRemoteSwitchV2.hpp>
+#include <BrickletRGBLED.hpp>
+#include <BrickletRGBLEDButton.hpp>
+#include <BrickletRGBLEDMatrix.hpp>
+#include <BrickletRGBLEDV2.hpp>
+#include <BrickletRotaryEncoder.hpp>
+#include <BrickletRotaryEncoderV2.hpp>
+#include <BrickletRotaryPoti.hpp>
+#include <BrickletRotaryPotiV2.hpp>
+#include <BrickletRS232.hpp>
+#include <BrickletRS232V2.hpp>
+#include <BrickletRS485.hpp>
+#include <BrickletSegmentDisplay4x7.hpp>
+#include <BrickletSegmentDisplay4x7V2.hpp>
+#include <BrickletSolidStateRelay.hpp>
+#include <BrickletSolidStateRelayV2.hpp>
+#include <BrickletSoundIntensity.hpp>
+#include <BrickletSoundPressureLevel.hpp>
+#include <BrickletTemperature.hpp>
+#include <BrickletTemperatureIR.hpp>
+#include <BrickletTemperatureIRV2.hpp>
+#include <BrickletTemperatureV2.hpp>
+#include <BrickletThermalImaging.hpp>
+#include <BrickletThermocouple.hpp>
+#include <BrickletThermocoupleV2.hpp>
+#include <BrickletTilt.hpp>
+#include <BrickletUVLight.hpp>
+#include <BrickletUVLightV2.hpp>
+#include <BrickletVoltage.hpp>
+#include <BrickletVoltageCurrent.hpp>
+#include <BrickletVoltageCurrentV2.hpp>
+#include <BrickletXMC1400Breakout.hpp>
+#include <BrickMaster.hpp>
+#include <BrickRED.hpp>
+#include <BrickServo.hpp>
+#include <BrickSilentStepper.hpp>
+#include <BrickStepper.hpp>
+#include <IPConnection.hpp>
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Registertinkerforgepackage
+{
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+extern DELPHI_PACKAGE void __fastcall Register();
+}	/* namespace Registertinkerforgepackage */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_REGISTERTINKERFORGEPACKAGE)
+using namespace Registertinkerforgepackage;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// RegistertinkerforgepackageHPP
