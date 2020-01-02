0f9cba0bb20aba604acee100e5e6c872f7f83044
Frédéric Libaud <frederic.libaud@numx-sets.eu>
2020-01-02 16:26:03 +0100

    commit: renaming of internal variables of methods and methods arguments

--- sources/BrickletRotaryEncoder.pas
+++ sources/BrickletRotaryEncoder.pas
@@ -25,8 +25,8 @@ type
   TArray0To2OfUInt8 = array [0..2] of byte;
 
   TBrickletRotaryEncoder = class;
-  TBrickletRotaryEncoderNotifyCount = procedure(aSender: TBrickletRotaryEncoder; const count: longint) of object;
-  TBrickletRotaryEncoderNotifyCountReached = procedure(aSender: TBrickletRotaryEncoder; const count: longint) of object;
+  TBrickletRotaryEncoderNotifyCount = procedure(aSender: TBrickletRotaryEncoder; const aCount: longint) of object;
+  TBrickletRotaryEncoderNotifyCountReached = procedure(aSender: TBrickletRotaryEncoder; const aCount: longint) of object;
   TBrickletRotaryEncoderNotifyPressed = procedure(aSender: TBrickletRotaryEncoder) of object;
   TBrickletRotaryEncoderNotifyReleased = procedure(aSender: TBrickletRotaryEncoder) of object;
 
@@ -60,7 +60,7 @@ type
     ///  Turning the encoder to the left decrements the counter,
     ///  so a negative count is possible.
     /// </summary>
-    function GetCount(const reset: boolean): longint; virtual;
+    function GetCount(const aReset: boolean): longint; virtual;
 
     /// <summary>
     ///  Sets the period with which the <see cref="BrickletRotaryEncoder.TBrickletRotaryEncoder.OnCount"/> callback is triggered
@@ -69,7 +69,7 @@ type
     ///  The <see cref="BrickletRotaryEncoder.TBrickletRotaryEncoder.OnCount"/> callback is only triggered if the count has changed since the
     ///  last triggering.
     /// </summary>
-    procedure SetCountCallbackPeriod(const period: longword); virtual;
+    procedure SetCountCallbackPeriod(const aPeriod: longword); virtual;
 
     /// <summary>
     ///  Returns the period as set by <see cref="BrickletRotaryEncoder.TBrickletRotaryEncoder.SetCountCallbackPeriod"/>.
@@ -136,7 +136,8 @@ type
     ///  The device identifier numbers can be found :ref:`here &lt;device_identifier&gt;`.
     ///  |device_identifier_constant|
     /// </summary>
-    procedure GetIdentity(out aUID: string; out connectedUid: string; out position: char; out hardwareVersion: TTFVersionNumber; out firmwareVersion: TTFVersionNumber; out deviceIdentifier: word); override;
+    procedure GetIdentity(out aUID: string; out aConnectedUID: string; out aPosition: char; out aHardwareVersion: TTFVersionNumber;
+                          out aFirmwareVersion: TTFVersionNumber; out aDeviceIdentifier: word); override;
 
     /// <summary>
     ///  This callback is triggered periodically with the period that is set by
@@ -199,28 +200,28 @@ begin
   aCallBacks[BRICKLET_ROTARY_ENCODER_CALLBACK_RELEASED]:= {$ifdef FPC}@{$endif}CallbackWrapperReleased;
 end;
 
-function TBrickletRotaryEncoder.GetCount(const reset: boolean): longint;
+function TBrickletRotaryEncoder.GetCount(const aReset: boolean): longint;
 var 
-_request, _response: TDynamicByteArray;
+  _request, _response: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_GET_COUNT, 9);
-  LEConvertBooleanTo(reset, 8, _request);
+  LEConvertBooleanTo(aReset, 8, _request);
   _response:= SendRequest(_request);
   Result:= LEConvertInt32From(8, _response);
 end;
 
-procedure TBrickletRotaryEncoder.SetCountCallbackPeriod(const period: longword);
+procedure TBrickletRotaryEncoder.SetCountCallbackPeriod(const aPeriod: longword);
 var 
-_request: TDynamicByteArray;
+  _request: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_SET_COUNT_CALLBACK_PERIOD, 12);
-  LEConvertUInt32To(period, 8, _request);
+  LEConvertUInt32To(aPeriod, 8, _request);
   SendRequest(_request);
 end;
 
 function TBrickletRotaryEncoder.GetCountCallbackPeriod: longword;
 var 
-_request, _response: TDynamicByteArray;
+  _request, _response: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_GET_COUNT_CALLBACK_PERIOD, 8);
   _response:= SendRequest(_request);
@@ -229,7 +230,7 @@ end;
 
 procedure TBrickletRotaryEncoder.SetCountCallbackThreshold(const aOption: char; const aMin: longint; const aMax: longint);
 var 
-_request: TDynamicByteArray;
+  _request: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_SET_COUNT_CALLBACK_THRESHOLD, 17);
   LEConvertCharTo(aOption, 8, _request);
@@ -240,7 +241,7 @@ end;
 
 procedure TBrickletRotaryEncoder.GetCountCallbackThreshold(out aOption: char; out aMin: longint; out aMax: longint);
 var 
-_request, _response: TDynamicByteArray;
+  _request, _response: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_GET_COUNT_CALLBACK_THRESHOLD, 8);
   _response:= SendRequest(_request);
@@ -251,7 +252,7 @@ end;
 
 procedure TBrickletRotaryEncoder.SetDebouncePeriod(const aDebounce: longword);
 var 
-_request: TDynamicByteArray;
+  _request: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_SET_DEBOUNCE_PERIOD, 12);
   LEConvertUInt32To(aDebounce, 8, _request);
@@ -260,7 +261,7 @@ end;
 
 function TBrickletRotaryEncoder.GetDebouncePeriod: longword;
 var 
-_request, _response: TDynamicByteArray;
+  _request, _response: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_GET_DEBOUNCE_PERIOD, 8);
   _response:= SendRequest(_request);
@@ -269,44 +270,47 @@ end;
 
 function TBrickletRotaryEncoder.IsPressed: boolean;
 var 
-_request, _response: TDynamicByteArray;
+  _request, _response: TDynamicByteArray;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_IS_PRESSED, 8);
   _response:= SendRequest(_request);
   Result:= LEConvertBooleanFrom(8, _response);
 end;
 
-procedure TBrickletRotaryEncoder.GetIdentity(out aUID: string; out connectedUid: string; out position: char; out hardwareVersion: TTFVersionNumber; out firmwareVersion: TTFVersionNumber; out deviceIdentifier: word);
+procedure TBrickletRotaryEncoder.GetIdentity(out aUID: string; out aConnectedUID: string; out aPosition: char; out aHardwareVersion: TTFVersionNumber; out aFirmwareVersion: TTFVersionNumber; out aDeviceIdentifier: word);
 var 
-_request, _response: TDynamicByteArray; _i: longint;
+  _request, _response: TDynamicByteArray;
+  _i: longint;
 begin
   _request:= IPConnection.CreateRequestPacket(self, BRICKLET_ROTARY_ENCODER_FUNCTION_GET_IDENTITY, 8);
   _response:= SendRequest(_request);
   aUID:= LEConvertStringFrom(8, 8, _response);
-  connectedUID:= LEConvertStringFrom(16, 8, _response);
-  position:= LEConvertCharFrom(24, _response);
-  for _i:= 0 to 2 do hardwareVersion[_i]:= LEConvertUInt8From(25 + (_i * 1), _response);
-  for _i:= 0 to 2 do firmwareVersion[_i]:= LEConvertUInt8From(28 + (_i * 1), _response);
-  deviceIdentifier:= LEConvertUInt16From(31, _response);
+  aConnectedUID:= LEConvertStringFrom(16, 8, _response);
+  aPosition:= LEConvertCharFrom(24, _response);
+  for _i:= 0 to 2 do aHardwareVersion[_i]:= LEConvertUInt8From(25 + (_i * 1), _response);
+  for _i:= 0 to 2 do aFirmwareVersion[_i]:= LEConvertUInt8From(28 + (_i * 1), _response);
+  aDeviceIdentifier:= LEConvertUInt16From(31, _response);
 end;
 
 procedure TBrickletRotaryEncoder.CallbackWrapperCount(const aPacket: TDynamicByteArray);
-var count: longint;
+var
+  _count: longint;
 begin
-  count:= LEConvertInt32From(8, aPacket);
+  _count:= LEConvertInt32From(8, aPacket);
 
   if (Assigned(countCallback)) then begin
-    countCallback(self, count);
+    countCallback(self, _count);
   end;
 end;
 
 procedure TBrickletRotaryEncoder.CallbackWrapperCountReached(const aPacket: TDynamicByteArray);
-var count: longint;
+var
+  _count: longint;
 begin
-  count:= LEConvertInt32From(8, aPacket);
+  _count:= LEConvertInt32From(8, aPacket);
 
   if (Assigned(countReachedCallback)) then begin
-    countReachedCallback(self, count);
+    countReachedCallback(self, _count);
   end;
 end;
 
