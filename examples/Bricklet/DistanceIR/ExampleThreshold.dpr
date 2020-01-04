8b2d964aa6c9005b428c67e6401eae049f53ca1c
Frédéric Libaud <frederic.libaud@numx-sets.eu>
2020-01-04 17:42:32 +0100

    commit: adding examples

---
+++ examples/Bricklet/DistanceIR/ExampleThreshold.dpr
@@ -0,0 +1,63 @@
+program ExampleThreshold;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletDistanceIR;
+
+type
+  TExample = class
+  private
+    ipcon: TIPConnection;
+    dir: TBrickletDistanceIR;
+  public
+    procedure DistanceReachedCB(sender: TBrickletDistanceIR; const distance: word);
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = 'XYZ'; { Change XYZ to the UID of your Distance IR Bricklet }
+
+var
+  e: TExample;
+
+{ Callback procedure for distance reached callback }
+procedure TExample.DistanceReachedCB(sender: TBrickletDistanceIR; const distance: word);
+begin
+  WriteLn(Format('Distance: %f cm', [distance/10.0]));
+end;
+
+procedure TExample.Execute;
+begin
+  { Create IP connection }
+  ipcon := TIPConnection.Createnil;
+
+  { Create device object }
+  dir := TBrickletDistanceIR.Create(UID, ipcon);
+
+  { Connect to brickd }
+  ipcon.Connect(HOST, PORT);
+  { Don't use device before ipcon is connected }
+
+  { Get threshold callbacks with a debounce time of 10 seconds (10000ms) }
+  dir.SetDebouncePeriod(10000);
+
+  { Register distance reached callback to procedure DistanceReachedCB }
+  dir.OnDistanceReached := {$ifdef FPC}@{$endif}DistanceReachedCB;
+
+  { Configure threshold for distance "smaller than 30 cm" }
+  dir.SetDistanceCallbackThreshold('<', 30*10, 0);
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy; { Calls ipcon.Disconnect internally }
+end;
+
+begin
+  e := TExample.Create;
+  e.Execute;
+  e.Destroy;
+end.
